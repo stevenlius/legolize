fcf07036c48a576da1ad65b59ad29136ad184ccd
fehy <fehyfehy@gmail.com>
2016-11-03 10:03:54 +0100

    dummyPointCloud

---
+++ Legolizer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Legolize;
+using PointCloudGen;
+
+namespace Legolizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Doer.Do(PointCloudGen.Generator.Cube(10, 8, 6));
+
+        }
+    }
+}
